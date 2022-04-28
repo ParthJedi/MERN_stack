746092f951d711c223352058e850c6c88faf98f7
ParthJedi <parth.00.joshi@gmail.com>
2022-04-28 10:33:37 +0530

    added registered user authentication

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
 const User = require('../../models/User');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const { check, validationResult } = require('express-validator');
+const config = require('config');
 
 //GET apis - /auth
 router.get('/', auth, async (req, res) => {
@@ -13,5 +17,62 @@ router.get('/', auth, async (req, res) => {
 		res.status(500).send('Server Error');
 	}
 });
+
+router.post(
+	'/',
+	[
+		check('email', 'Please enter a valid email').isEmail(),
+		check('password', 'Password is required ').exists(),
+	],
+	async (req, res) => {
+		try {
+			const errors = validationResult(req);
+			if (!errors.isEmpty()) {
+				return res.status(400).json({
+					errors: errors.array(),
+				});
+			}
+			const { email, password } = req.body;
+
+			try {
+				const user = await User.findOne({ email });
+				if (!user) {
+					return res
+						.status(400)
+						.json({ errors: [{ message: 'Invalid Credentials' }] });
+				}
+
+				const isMatch = await bcrypt.compare(password, user.password);
+
+				if (!isMatch) {
+					return res
+						.status(400)
+						.json({ errors: [{ message: 'Invalid Credentials' }] });
+				}
+
+				const payload = {
+					user: {
+						id: user.id,
+					},
+				};
+				jwt.sign(
+					payload,
+					config.get('jwtSecret'),
+					{ expiresIn: '36000000' },
+					(err, token) => {
+						if (err) throw err;
+						res.send(token);
+					}
+				);
+			} catch (error) {
+				console.error(error.message);
+				res.status(500).send('Server Error1');
+			}
+		} catch (err) {
+			console.error(err.message);
+			res.status(500).send('Server Error');
+		}
+	}
+);
 
 module.exports = router;
