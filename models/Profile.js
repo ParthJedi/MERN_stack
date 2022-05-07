15e7a61e5fccb0414c12022e3a1ff3ea91990b48
ParthJedi <parth.00.joshi@gmail.com>
2022-05-07 11:36:03 +0530

    profile model

--- models/Profile.js
+++ models/Profile.js
@@ -1,26 +1,112 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({
-	name: {
-		type: String,
+const profileSchema = new mongoose.Schema({
+	user: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'user',
+	},
+	company: {
+		type: string,
+	},
+	website: {
+		type: string,
+	},
+	location: {
+		type: string,
+	},
+	status: {
+		type: string,
 		required: true,
 	},
-	email: {
-		type: String,
-		required: true,
-		unique: true,
-	},
-	password: {
-		type: String,
+	skills: {
+		type: [string],
 		required: true,
 	},
-	avatar: {
-		type: String,
+	bio: {
+		type: string,
 	},
-	date: {
+	githubUsername: {
+		type: string,
+	},
+	experience: [
+		{
+			title: {
+				type: string,
+				required: true,
+			},
+			company: {
+				type: string,
+				required: true,
+			},
+			location: {
+				type: string,
+			},
+			from: {
+				type: Date,
+				required: true,
+			},
+			to: {
+				type: Date,
+			},
+			current: {
+				type: Boolean,
+				default: false,
+			},
+			description: {
+				type: string,
+			},
+		},
+	],
+	education: [
+		{
+			school: {
+				type: string,
+				required: true,
+			},
+			degree: {
+				type: string,
+				required: true,
+			},
+			fieldOfStudy: {
+				type: string,
+				required: true,
+			},
+			from: {
+				type: Date,
+				required: true,
+			},
+			to: {
+				type: Date,
+			},
+			current: {
+				type: Boolean,
+			},
+			description: {
+				type: string,
+			},
+		},
+	],
+	social: {
+		youtube: {
+			type: string,
+		},
+		twitter: {
+			type: string,
+		},
+		facebook: {
+			type: string,
+		},
+		linkedin: {
+			type: string,
+		},
+		instagram: {
+			type: string,
+		},
+	},
+	Date: {
 		type: Date,
-		default: Date.now(),
+		default: Date.now,
 	},
 });
 
-module.exports = User = mongoose.model('user', userSchema);
+module.exports = Profile = mongoose.model('profile', profileSchema);
