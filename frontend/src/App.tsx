6cbee5936339ca1b0bd3a649c53d020deb3ff4ce
ParthJedi <parth.00.joshi@gmail.com>
2022-05-16 22:35:58 +0530

    setting up the redux store

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+import Alert from './components/layout/Alert';
 import './App.css';
 // redux
 import { Provider } from 'react-redux';
@@ -18,6 +19,7 @@ const App = () => {
 					<Routes>
 						<Route path='/' element={<Landing />} />
 					</Routes>
+					<Alert />
 					<section className='container'>
 						<Routes>
 							<Route path='/register' element={<Register />} />
