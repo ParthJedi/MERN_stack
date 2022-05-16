e5da19ada8d95164f0cec14d069851aeab6e531f
ParthJedi <parth.00.joshi@gmail.com>
2022-05-16 12:18:27 +0530

    UI - register a user

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,8 @@ import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
 import './App.css';
 
 const App = () => {
@@ -12,6 +14,12 @@ const App = () => {
 				<Routes>
 					<Route path='/' element={<Landing />} />
 				</Routes>
+				<section className='container'>
+					<Routes>
+						<Route path='/register' element={<Register />} />
+						<Route path='/login' element={<Login />} />
+					</Routes>
+				</section>
 			</Router>
 		</Fragment>
 	);
