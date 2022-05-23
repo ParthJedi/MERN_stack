8b55ea574e6d1f6c3b2c8f95a034c465c2130d8d
ParthJedi <parth.00.joshi@gmail.com>
2022-05-23 19:00:32 +0530

    set auth token

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import React, { Fragment, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
+import Dashboard from './components/dashboard/Dashboard';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import Alert from './components/layout/Alert';
@@ -33,6 +34,7 @@ const App = () => {
 						<Routes>
 							<Route path='/register' element={<Register />} />
 							<Route path='/login' element={<Login />} />
+							<Route path='/dashboard' element={<Dashboard />} />
 						</Routes>
 					</section>
 				</Router>
